f606f5a09c6f784ed12d66a7caaf3e38fca17cde
marucs-ludwig <mamanaytay@cvisc.pshs.edu.ph>
2026-09-17 15:04:26 +0800

    Add files via upload

---
+++ Favorite season.py
@@ -0,0 +1,7 @@
+V_s = ["Spring", "Summer", "Autumn", "Winter"]
+
+season = input("Enter your favorite season: ")
+if season in V_s:
+    print(f"Your favorite season is: {season}")
+else:
+    print("Invalid season!")
